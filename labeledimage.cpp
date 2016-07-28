f2199de1fdfbdbcf123d35bb81ffc62c825f1923
PentaHiggs <Ichiroska89@yahoo.com>
2016-07-28 18:54:30 -0400

    Modified the function doing matrix arithmetic in LabeledImage to findRectTransform, taking a bRect as an input.  Now it properly calculates the correct transform in projective space from the bRect to the arbritrary quadrilateral defined by the given QPoints instead of calculating the transform from the unit square to the quadrilateral.

--- labeledimage.cpp
+++ labeledimage.cpp
@@ -34,16 +34,16 @@ virtual void LabeledImage::mouseReleaseEvent(QGraphicsSceneHoverEvent *event){
     QGraphicsPixmapItem::mouseReleaseEvent(event);
 }
 
-bRectTransform LabeledImage::setRectTransform(QPoint A, QPoint B, QPoint C, QPoint D){
+bRectTransform LabeledImage::findRectTransform(bRect R, QPoint A, QPoint B, QPoint C, QPoint D){
     Eigen::Matrix<float, 8, 8> k;
-    k <<    0,  0,  1,  0,  0,  0,  0,          0,
-            0,  0,  0,  0,  0,  1,  0,          0,
-            0,  1,  1,  0,  0,  0,  0,          (-1)*B.x(),
-            0,  0,  0,  0,  1,  1,  0,          (-1)*B.y(),
-            1,  1,  1,  0,  0,  0,  (-1)*C.x(), (-1)*C.x(),
-            0,  0,  0,  1,  1,  1,  (-1)*C.y(), (-1)*C.y(),
-            1,  0,  1,  0,  0,  0,  (-1)*D.x(), 0,
-            0,  0,  0,  1,  0,  1,  (-1)*D.y(), 0;
+    k <<    R.x,    R.y,    1,      0,      0,      0,      (-1)*R.x*A.x(),         (-1)*R.y*A.x(),
+            0,      0,      0,      R.x,    R.y,    1,      (-1)*R.x*A.y(),         (-1)*R.y*A.y(),
+            R,x,    R.y+R.h,1,      0,      0,      0,      (-1)*R.x*B.x(),         (-1)*(R.y+R.h)*B.x(),
+            0,      0,      0,      R.x,    R.y+R.h,1,      (-1)*R.x*B.y(),         (-1)*(R.y+R.h)*B.y(),
+            R.x+R.w,R.y+R.h,1,      0,      0,      0,      (-1)*(R.x+R.w)*C.x(),   (-1)*(R.y+R.h)*C.x(),
+            0,      0,      0,      R.x+R.w,R.y+R.h,1,      (-1)*(R.x+R.w)*C.y(),   (-1)*(R.y+R.h)*C.y(),
+            R.x+R.w,R.y,    1,      0,      0,      0,      (-1)*(R.x+R.w)*D.x(),   (-1)*R.y*D.x(),
+            0,      0,      0,      R.x+R.w,R.y,    1,      (-1)*(R.x+R.w)*D.y(),   (-1)*R.y*D.y();
 
     Eigen::Matrix<float, 8, 8> inverseK;
     bool invertible;
